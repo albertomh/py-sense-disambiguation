e5724db10ecc1f7244265f24c56ab370496597d3
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2016-04-02 14:11:19 -0400

    create cleanData()

--- classifier.py
+++ classifier.py
@@ -30,3 +30,22 @@ def dictTag():
         d_id[v_id.group(0)] = v_tag.group(1)
 
     return d_id
+
+
+def cleanData():
+
+    d_clean = dict()
+    l_pairs = list()
+    l_clean = getData()
+    l_stop = ["he", "she", "and", "it", "its", "that", "to", "a", "or", "by", "mine", "my", "your", "yours", "their", "theirs", "theyre", "he", "she", "them", "is", "are", "we", "our", "ours", "his", "her", "i", "if", "but", "be", "will", "the", "like", "has", "have", "mr", "mrs", "with", "not", "in", "of", "for", "was", "when", "they", "all", "an", "were", "had", "also", "as", "no", "what", "tag", "from", "on", "dr", "do", "this", "at", "even"]
+
+    for i in range(len(l_clean)):
+        s_id = re.search("\"(\d{6})", l_clean[i]).group(1)
+        l_clean[i] = re.sub("\d|\\n|\&.*?\.|\'s", "", l_clean[i])
+        l_clean[i] = l_clean[i].translate(None, string.punctuation)
+        l_clean[i] = re.sub("  | tag | \w{1} ", " ", l_clean[i])
+        l_clean[i] = [word for word in l_clean[i].split() if word not in l_stop]
+
+        l_pairs.append((s_id, l_clean[i]))
+
+    return l_pairs
